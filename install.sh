e9e0b8607606ea2669a7d798fa72c581cbfdff7d
Derek <derek.peck@parivedasolutions.com>
2018-02-15 09:05:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 yum update -y
 
 yum install java-1.8.0 -y
-yum remove java-1.7.0-openjdk
+yum remove java-1.7.0-openjdk -y
 
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
